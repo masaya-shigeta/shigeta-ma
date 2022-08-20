eb16ff30cfabee56087776416a8128ede7ce6a11
masaya-shigeta <msy.marumaru@gmail.com>
2022-08-20 21:54:49 +0900

    アイコン画像追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,14 @@ const Home: NextPage = () => {
     <>
       <div className="h-12"></div>
       <div className="mx-auto max-w-3xl px-6">
-        <div className="w-20 h-20 bg-white mx-auto"></div>
+        <div className="w-40 mx-auto">
+          <img
+            src="/img/profile.png"
+            alt="アイコン"
+          />
+        </div>
       </div>
-      <div className="mx-auto my-10 max-w-3xl px-6 text-center text-white text-2xl">
+      <div className="mx-auto my-2 max-w-3xl px-6 text-center text-white text-2xl">
         MASAYA SHIGETA
       </div>
       <div className="h-full bg-gray-50"></div>
